3e088dfc29ee36296efad5eea1fbab13f5d4f3e1
temet_nosce <akifzade.ferid1995@gmail.com>
2019-10-28 16:45:04 -0700

    -- added null protection

--- src/lesson8/WarmUp1.java
+++ src/lesson8/WarmUp1.java
@@ -4,7 +4,6 @@ import java.util.ArrayList;
 
 class Task
 {
-    // 1 2z 3 = 4
     private String string = "";
 
     public Task(String string) {
@@ -21,20 +20,23 @@ class Task
     }
     private ArrayList<String> split()
     {
-        ArrayList<String> result = new ArrayList<>();
-        char[] chArray = this.string.toCharArray();
-        String result1 = "";
-        for (char ch: chArray) {
-            if(ch != ' ')
-                result1 += ch;
-            else {
-                result.add(result1);
-                result1 = "";
-            }
+        if(this.string != null) {
+            ArrayList<String> result = new ArrayList<>();
+            char[] chArray = this.string.toCharArray();
+            String result1 = "";
+            for (char ch : chArray) {
+                if (ch != ' ')
+                    result1 += ch;
+                else {
+                    result.add(result1);
+                    result1 = "";
+                }
 
+            }
+            result.add(result1);
+            return result;
         }
-        result.add(result1);
-        return result;
+        throw new NullPointerException("String in class cannot be null");
     }
     public Integer solve()
     {
